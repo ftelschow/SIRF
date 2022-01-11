95f3028feb751d9f0ae63799412a4213e64a9f2b
Fabian Telschow <fabian.telschow@hu-berlin.de>
2022-01-11 09:56:26 +0100

    file for simulation on server

---
+++ Code_Articles/FunctionalDeltaResiduals/Parallelize_SCB_sim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for parallel_count in {1..20}
+do
+for model in "ModelA" "ModelB" "ModelC"
+do
+for transform in "skewness" "skewnessN" "kurtosis" "kurtosisN"
+do
+	nohup Rscript --vanilla Sim_Script.R $model 500 $transform $parallel_count "2022_01_11" "Rpackages/SIRF/Code_Articles/FunctionalDeltaResiduals/" &
+done
+done
+done
