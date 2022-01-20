721994ee3fd23cdfa15ef762fcd562e2d064db1d
Fabian Telschow <fabian.telschow@hu-berlin.de>
2022-01-20 14:51:24 +0100

    test

--- Code_Articles/FunctionalDeltaResiduals/Parallelize_SCB_sim_ModelA_obs.sh
+++ Code_Articles/FunctionalDeltaResiduals/Parallelize_SCB_sim_ModelA_obs.sh
@@ -5,7 +5,10 @@ for x in 50 100 175
 do
 for obs in 0.05 0.1
 do
-	nohup Rscript --vanilla Sim_Script2.R "ModelA" 500 $parallel_count $x $obs "2021_11_19" "~/Rpackages/SIRF/Code_Articles/FunctionalDeltaResiduals/" &
+for transform in "cohensd" "skewness" "skewness (normality)"
+do
+	nohup Rscript --vanilla Sim_Script_obs.R "ModelA" 500 $parallel_count $x $obs "2022_01_20" "~/Rpackages/SIRF/Code_Articles/FunctionalDeltaResiduals/" $transform &
+do
 done
 done
 done
