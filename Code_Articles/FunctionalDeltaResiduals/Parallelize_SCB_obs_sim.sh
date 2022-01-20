da0fa4924f6a3f3aa83053ad0c855278156d79db
Fabian Telschow <fabian.telschow@hu-berlin.de>
2022-01-20 14:50:44 +0100

    new files for observation noise simulations

---
+++ Code_Articles/FunctionalDeltaResiduals/Parallelize_SCB_obs_sim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for parallel_count in {1..20}
+do
+for model in "ModelA" "ModelB"
+do
+for transform in "cohensd" "skewness" "skewnessN"
+do
+for obs in 0.05 0.1
+do
+	nohup Rscript --vanilla Sim_Script_obs.R $model 250 $parallel_count $x $obs "2022_01_20" "~/Rpackages/SIRF/Code_Articles/FunctionalDeltaResiduals/"  $transform &
+done
+done
+done
+done
