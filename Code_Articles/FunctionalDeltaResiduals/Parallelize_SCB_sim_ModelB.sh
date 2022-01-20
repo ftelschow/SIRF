678afac6cf4efd35c426280746d9351ef06c4b0b
Fabian Telschow <fabian.telschow@hu-berlin.de>
2022-01-20 14:58:17 +0100

    removed

--- Code_Articles/FunctionalDeltaResiduals/Parallelize_SCB_sim_ModelB.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for parallel_count in {1..20}
-do
-  nohup Rscript --vanilla Sim_Script.R "ModelB" 500 $parallel_count "2021_05_20" "~/Rpackages/SIRF/Code_Articles/FunctionalDeltaResiduals/" &
-done
--- Code_Articles/FunctionalDeltaResiduals/Parallelize_SCB_sim_ModelC.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for parallel_count in {1..20}
-do
-  nohup Rscript --vanilla Sim_Script.R "ModelC" 500 $parallel_count "2021_11_15" "~/Rpackages/SIRF/Code_Articles/FunctionalDeltaResiduals/" &
-done
-
-
