8be0fb7f3d4a8f1e3c26bb3b90e46c439b4ad29d
Fabian Telschow <fabian.telschow@hu-berlin.de>
2022-01-20 14:55:00 +0100

    added skewness normality simulation

--- Code_Articles/FunctionalDeltaResiduals/Variance_SkewnessKurtosis_estimates.R
+++ Code_Articles/FunctionalDeltaResiduals/Variance_SkewnessKurtosis_estimates.R
@@ -17,6 +17,8 @@ Vibrant = c( "#0077BB", "#33BBEE", "#009988",
              "#EE7733", "#CC3311", "#EE3377",
              "#BBBBBB" )
 
+path_wd   <- "/home/fabian/Seafile/Code/Rpackages/SIRF/Code_Articles/FunctionalDeltaResiduals/"
+
 path_pics = "/home/fabian/Seafile/Projects/2019_DeltaResiduals/Pics/"
 
 
@@ -24,7 +26,7 @@ path_pics = "/home/fabian/Seafile/Projects/2019_DeltaResiduals/Pics/"
 # General Simulation setup
 #-------------------------------------------------------------------------------
 # General Simulation parameters
-Msim    = 5e1 # number of simulations
+Msim    = 5e3 # number of simulations
 Nvec    = c(50, 100, 200, 400, 800) # sample sizes considered
 x       = seq(0, 1, length.out = 175) # locations the process is evaluated at
 
@@ -55,7 +57,7 @@ trueVar_Gauss <- function(Nvec, method, x, mu = NULL, sigma = NULL){
   if(method == "cohensd"){
     nu  = Nvec - 1
     d  = function(v)  sqrt(v) * mu(x) / sigma(x)
-    
+
     trueVar = t(vapply(nu, function(v) v * (1 + d(v)^2) / (v - 2) -d(v)^2 *
                                            (1 - 3 / (4 * v -1 ))^(-2),
                        FUN.VALUE = rep(1, length(x))
@@ -67,7 +69,7 @@ trueVar_Gauss <- function(Nvec, method, x, mu = NULL, sigma = NULL){
                        (Nvec - 2) / (Nvec + 3) / (Nvec + 5), length(Nvec), length(x))
   }
   rownames(trueVar) <- Nvec
-  
+
   return(trueVar)
 }
 
@@ -75,7 +77,7 @@ trueVar_Gauss <- function(Nvec, method, x, mu = NULL, sigma = NULL){
 plot_var_ests <- function(method, x, estVar, trueVar, outname = NULL){
   relBias = ( estVar - trueVar ) / trueVar
   NN      = dim(estVar)[1]
-  
+
   if(is.null(outname)){
     par(mfrow = c(1,2))
     plot( NULL,
@@ -97,7 +99,7 @@ plot_var_ests <- function(method, x, estVar, trueVar, outname = NULL){
           ylab = paste("variance (", method,")", sep = ""),
           main = "Estimated vs true variance" )
     matlines( x, t(estVar), lty = 1, col = 1:NN )
-    matlines( x, t(trueVar), lty = 2, col = 1:NN )    
+    matlines( x, t(trueVar), lty = 2, col = 1:NN )
   }else{
     png( filename = paste( filename, "_", method, ".png" , sep = "" ),
          width = 1000,
@@ -124,11 +126,12 @@ plot_var_ests <- function(method, x, estVar, trueVar, outname = NULL){
           main = "Estimated vs true variance" )
     matlines( x, t(estVar), lty = 1, col = 1:NN )
     matlines( x, t(trueVar), lty = 2, col = 1:NN )
-    dev.off() 
+    dev.off()
   }
 }
 
 methodVec = c("cohensd", "skewness", "kurtosis")
+methodVec = c("skewness (normality)")
 
 #-------------------------------------------------------------------------------
 # Simulation model A
@@ -143,22 +146,55 @@ filename = paste(path_pics, "DeltaResidualEstimateVariance" , sep = "")
 filename = NULL
 
 
-# Simulate the variance
-for(method in methodVec){
+# Simulate the variance of cohens d
+method = "cohensd"
   sim_var_d <- sim_varEsts(Msim, method = method, Nvec, x,
                            mu = mu_modelA,
                            noise = noise_modelA,
                            sigma = sigma_modelA)
-  
+
   # Get the mean of the simulated variance and the relative bias
   estVar = apply( sim_var_d, 1:2, mean )
   rownames(estVar) <- Nvec
   trueVar <- trueVar_Gauss(Nvec, method = method, x, mu = mu_modelA, sigma = sigma_modelA)
-  
-  
+
   # Plot the data
-  plot_var_ests(method = method, x, estVar, trueVar, outname = filename)  
-}
+  plot_var_ests(method = method, x, estVar, trueVar, outname = filename)
+
+
+  # Simulate the variance of skewness
+method = "skewness"
+  sim_var_s <- sim_varEsts(Msim, method = method, Nvec, x,
+                           mu = mu_modelA,
+                           noise = noise_modelA,
+                           sigma = sigma_modelA)
+
+  # Get the mean of the simulated variance and the relative bias
+  estVar = apply( sim_var_s, 1:2, mean )
+  rownames(estVar) <- Nvec
+  trueVar_s <- trueVar_Gauss(Nvec, method = method, x, mu = mu_modelA, sigma = sigma_modelA)
+
+  # Plot the data
+  plot_var_ests(method = method, x, estVar, trueVar, outname = filename)
+
+
+  # Simulate the variance of skewness (normalization)
+method = "skewness (normality)"
+  sim_var_sn <- sim_varEsts(Msim, method = method, Nvec, x,
+                           mu = mu_modelA,
+                           noise = noise_modelA,
+                           sigma = sigma_modelA)
+
+  # Get the mean of the simulated variance and the relative bias
+  estVar_sn = apply( sim_var_sn, 1:2, mean )
+  rownames(estVar_sn) <- Nvec
+  trueVar_sn <- rep(1, length.out = length(x))
+
+  # Plot the data
+  plot_var_ests(method = method, x, estVar, trueVar, outname = filename)
+
+
+  save.image(paste(path_wd, "Workspaces/Variance_Simulation2.Rdata", sep = ""))
 
 
 #-------------------------------------------------------------------------------
@@ -179,39 +215,13 @@ for(method in methodVec){
                            mu = mu_modelA,
                            noise = noise_modelA,
                            sigma = sigma_modelA)
-  
+
   # Get the mean of the simulated variance and the relative bias
   estVar = apply( sim_var_d, 1:2, mean )
   rownames(estVar) <- Nvec
   trueVar <- trueVar_Gauss(Nvec, method = method, x, mu = mu_modelA, sigma = sigma_modelA)
-  
-  
+
+
   # Plot the data
-  plot_var_ests(method = method, x, estVar, trueVar, outname = NULL)  
-}
-
-
-#-------------------------------------------------------------------------------
-# Simulation model B
-#-------------------------------------------------------------------------------
-# Functional data model
-mu_modelA    = function(x){sin(4 * pi * x) * exp(-3 * x)}
-sigma_modelA = function(x){((1 - x - 0.4)^2 + 1) / 6}
-noise_modelA = RandomNormalSum
-
-# Simulate the variance
-for(method in methodVec){
-  sim_var_d <- sim_varEsts(Msim, method = method, Nvec, x,
-                           mu = mu_modelA,
-                           noise = noise_modelA,
-                           sigma = sigma_modelA)
-  
-  # Get the mean of the simulated variance and the relative bias
-  estVar = apply( sim_var_d, 1:2, mean )
-  rownames(estVar) <- Nvec
-  trueVar <- trueVar_Gauss(Nvec, method = method, x, mu = mu_modelA, sigma = sigma_modelA)
-  
-  
-  # Plot the data
-  plot_var_ests(method = method, x, estVar, trueVar, outname = NULL)  
+  plot_var_ests(method = method, x, estVar, trueVar, outname = NULL)
 }
