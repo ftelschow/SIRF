17d63badd701e9645688701c74282111871c7db7
Fabian Telschow <fabian.telschow@hu-berlin.de>
2022-09-06 17:11:21 +0200

    removed small bugs

--- R/CoPEFunctions.R
+++ R/CoPEFunctions.R
@@ -4,116 +4,12 @@
 #                                                                              #
 #------------------------------------------------------------------------------#
 # Contained functions:
-#  - scb_moments
+#
 #
 #------------------------------------------------------------------------------#
 # Developer notes:
-# - Style guideline included
-# - add method description
+#
 #------------------------------------------------------------------------------#
-#' Estimates the .
-#'
-#' @param hatmu function to be thresholded to estimated the set SC. Usually it is
-#' an estimator of the signal
-#' @param C T x 2 matrix containing in the first column the lower bound for the
-#' tube and in the second column the upper bound.
-#' @param hatsigma standard deviation used to estimate the set SC. Usually it is
-#' the pointwise standard deviation
-#' @param tN is inverse of the rate of the fCLT, i.e., usually 1/sqrt(N)
-#' @param  kN is the thickening of the set (Default log(tN^-2) / 5)
-#' @param  type if CoPE sets are computed either "selection" or
-#' "extraction"(Default ). Otherwise if it is estimated for a statistical test
-#' using CoPE sets it is "rel" (relevant tube test), "out" (outside relevant tube
-#'  test), "eqv" (bio-equivalence test)
-#' @return list with elements
-#'  \itemize{
-#'   \item hatmean pointwise sample mean
-#'   \item scb list containing the upper and lower bounds of the simultaneous confidence band
-#'   \item level targeted covering probability
-#'   \item q quantile of the maximum of the residual field
-#' }
-#' @export
-PreimageC <- function(hatmu, C, hatsigma, tN, kN = log(tN^-2) / 5, type = "extraction"){
-  dC = dim(C)
-  if(type %in% c("rel", "out", "eqv")){
-    # Construct the threshold of the CoPE set
-    if(type == "rel"){
-      Delta = min(min(abs(C[,1] - hatmu)), min(abs(hatmu - C[,dC[2]])))
-      cm = C[,1] + Delta
-      cp = C[,dC[2]] - Delta
-
-      updn <- c(diff(sign(hatmu-C[,1])), 0)
-      ix <- which(updn != 0)
-      ixm = vapply(ix, function(v){ ifelse( which.min(c(abs(hatmu[v]-C[v,1]),
-                                      abs(hatmu[v+1]-C[v+1,1]))) == 1,
-                                      v, v+1)}, FUN.VALUE = 1 )
-      updn <- c(diff(sign(hatmu-C[,dC[2]])), 0)
-      ix <- which(updn != 0)
-      ixp = vapply(ix, function(v){ ifelse( which.min(c(abs(hatmu[v]-C[v,dC[2]]),
-                                                        abs(hatmu[v+1]-C[v+1,dC[2]]))) == 1,
-                                            v, v+1)}, FUN.VALUE = 1 )
-
-    }else if(type == "out"){
-      Delta = min(min(abs(C[,1] - hatmu)), min(abs(hatmu - C[,dC[2]])))
-      cm = C[,1] - Delta
-      cp = C[,dC[2]] + Delta
-      ixm <- ixp <- NULL
-    }else if(type == "eqv"){
-      Delta = max(max(C[,1] - hatmu), max(hatmu - C[,dC[2]]))
-      cm = C[,1] - Delta
-      cp = C[,dC[2]] + Delta
-      ixm <- ixp <- NULL
-    }
-
-    # Get the estimates of the set
-    S_cm = ( cm - tN * kN * hatsigma <= hatmu) & (hatmu <= cm + tN * kN * hatsigma)
-    S_cp = ( cp - tN * kN * hatsigma <= hatmu) & (hatmu <= cp + tN * kN * hatsigma)
-
-    S_cm[ixm] <- TRUE
-    S_cp[ixp] <- TRUE
-    # Output the set S_p and S_m
-    list(
-      S_m = S_cm,
-      S_p = S_cp,
-      c_m = cm,
-      c_p = cp,
-      bd  = tN * kN * hatsigma
-    )
-  }else if(type == "extraction"){
-    M = (C - tN * kN * hatsigma <= hatmu) & (hatmu <= C + tN * kN * hatsigma)
-    out = list(
-      S_p = M[,1],
-      S_m = M[,dC[2]]
-    )
-    return( out )
-    # return( apply(M, 1, any) )
-  }else if(type == "selection"){
-    return( (C[,1] - tN * kN * hatsigma <= hatmu) & (hatmu <= C[,dC[2]] + tN * kN * hatsigma) )
-  }
-}
-
-#' Estimates the critical set for the Dette papers.
-#' @inheritParams PreimageC
-#' @return list with elements
-#'  \itemize{
-#'   \item hatmean pointwise sample mean
-#'   \item scb list containing the upper and lower bounds of the simultaneous confidence band
-#'   \item level targeted covering probability
-#'   \item q quantile of the maximum of the residual field
-#' }
-#' @export
-# hat Extremal sets E_pm
-hatE_pm <- function(hatmu, C, hatsigma, tN, kN = log(tN^-2) / 5){
-  T_hatmu = max(max(-hatmu + C[,1]), max(hatmu - C[,2]))
-
-  out = list(
-    S_p =  hatmu - C[,2] >= T_hatmu - tN * kN * hatsigma,
-    S_m = -hatmu + C[,1] >= T_hatmu - tN * kN * hatsigma
-  )
-  return(out)
-}
-
-
 #' This functions computes inner CoPE set
 #'
 #' @inheritParams PreimageC
@@ -298,7 +194,7 @@ CoPE_inference <- function(hatmu, hatsigma, R, x = seq(0,1, length(hatmu)),
   # Estimate the quantile. This requires estimation of the critical set
   if(type == "extraction"){
     SC = PreimageC(hatmu = estmu, C = C, hatsigma = hatsigma,
-                   tN = tN, kN = kN, type = "rel")
+                   tN = tN, kN = kN, type = "extraction")
     Splus  = SC$S_p
     Sminus = SC$S_m
 
@@ -361,20 +257,21 @@ CoPE_inference <- function(hatmu, hatsigma, R, x = seq(0,1, length(hatmu)),
     colVecSet[SC] = "red"
 
   }else if(type == "classical"){
+    # Estimate the critial set
     SC = PreimageC(hatmu = estmu, C = C, hatsigma = hatsigma,
-                   tN = tN, kN = kN, type = "rel")
-    SCC = SC$S_p | SC$S_m
-    if(sum(SCC) == 1){
+                   tN = tN, kN = kN, type = "selection")
+
+    if(sum(SC) == 1){
       # Get the threshold
-      q = MultiplierBootstrap(R = t(R[SCC,]),
+      q = MultiplierBootstrap(R = t(R[SC,]),
                               Mboots = Mboots,
                               alpha  = alpha,
                               method  = method,
                               weights = weights)
 
-    }else if(sum(SCC) > 1){
+    }else if(sum(SC) > 1){
       if(is.null(fair)){# unfair threshold
-        q = MultiplierBootstrap(R = t(t(R[SCC,])),
+        q = MultiplierBootstrap(R = t(t(R[SC,])),
                                 Mboots = Mboots,
                                 alpha  = alpha,
                                 method  = method,
@@ -388,8 +285,8 @@ CoPE_inference <- function(hatmu, hatsigma, R, x = seq(0,1, length(hatmu)),
                                 weights = weights)
         test = OptimizeFairThreshold1D(q$samples, x, fair,
                                        fair.type = fair.type,
-                                       Splus  = SCC,
-                                       Sminus = SCC,
+                                       Splus  = SC,
+                                       Sminus = SC,
                                        alpha  = alpha,
                                        niter  = niter,
                                        print.coverage = print.coverage,
@@ -403,7 +300,7 @@ CoPE_inference <- function(hatmu, hatsigma, R, x = seq(0,1, length(hatmu)),
     }
     # ColorVector of estimated set
     colVecSet = rep("black", length(x))
-    colVecSet[SC$S_m | SC$S_p] = "red"
+    colVecSet[SC] = "red"
   }else if(type == "SCB"){
     # Get the unfair threshold
     q = MultiplierBootstrap(R = R,
@@ -473,7 +370,6 @@ CoPE_inference <- function(hatmu, hatsigma, R, x = seq(0,1, length(hatmu)),
          fair       = fair,
          fair.type  = fair.type)
   }
-
 }
 
 #' This functions enables different inference for 1D fields using
