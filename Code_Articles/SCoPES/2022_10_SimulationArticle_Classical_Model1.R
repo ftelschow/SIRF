d8416d3ecaf5d39ed53d8529c03537b1b2a0dd9f
Fabian Telschow <fabian.telschow@hu-berlin.de>
2022-11-22 17:30:31 +0100

    small adaptations

--- Code_Articles/SCoPES/2022_10_SimulationArticle_Classical_Model1.R
+++ Code_Articles/SCoPES/2022_10_SimulationArticle_Classical_Model1.R
@@ -4,14 +4,14 @@
 # Prepare workspace
 rm(list = ls())
 
-setwd("~/Rpackages/SIRF/Code_Articles/SCoPES")
+setwd("/home/fabian/Seafile/Code/Rpackages/SIRF/Code_Articles/SCoPES")
 
 library(SampleFields)
 library(tidyverse)
 library(SIRF)
 
 source("Auxillary_fcns.R")
-today = "2022_11_9_"
+today = "2022_22_9_"
 #-------------------------------------------------------------------------------
 mu_name = "1" # "2" #
 SCoPEStype = "classical" # "extraction"
@@ -22,28 +22,20 @@ alpha = 0.1
 C     = c(0,3)
 
 NVec    = c(20, 50, 1e2, 2e2, 5e2, 10e2)
-betaVec = c(0.99, 0.2, 0.5, 0.15, 0.1, 0.05)
-
-# Model parameters
-if(mu_name == "1"){
-  NDelta = c(30, 20, 30, 0, 0)
-}else{
-  NDelta = c(0, 80, 0, 0, 0)
-}
+betaVec = c(1-alpha, 0.01, 2, 5, 10, 15, 666)
 
 # variables: q estimation
 name       = "t" # "gauss" # "mboot" #
-mu1est     = "thickening" # NULL #
 
 # Output containers
 lN = length(NVec)
 bN = length(betaVec)
-coverage         <- matrix(NaN, bN, lN)
-trueDetect_holm  <- trueDetect_sidak <- trueDetect_scopes <- array(NaN, dim = c(bN, lN, Msim))
-falseDetect_holm <- falseDetect_sidak <- falseDetect_scopes <- array(NaN, dim = c(bN, lN, Msim))
+coverage       <- matrix(NaN, bN, lN)
+trueDetect_BH  <- trueDetect_holm  <- trueDetect_sidak <- trueDetect_scopes <- array(NaN, dim = c(bN, lN, Msim))
+falseDetect_BH <- falseDetect_holm <- falseDetect_sidak <- falseDetect_scopes <- array(NaN, dim = c(bN, lN, Msim))
 
 # Model specification
-muvec = generate_muvec(NDelta)
+muvec = generate_muvec(mu_name)
 model <- list(
   x  = 1:length(muvec),
   mu = Vectorize(function(x){ t = muvec; return(t[x]) }),
@@ -54,13 +46,22 @@ model <- list(
 
 # Variables for the tubes
 if(SCoPEStype == "classical"){
-  C = rep(0, sum(NDelta))
+  C = rep(0, length(muvec))
 }else{
-  C = cbind(rep(0, sum(NDelta)), rep(3, sum(NDelta)))
+  C = cbind(rep(0, length(muvec)), rep(3, length(muvec)))
 }
 
 for(n in 1:lN){for(b in 1:bN){
-  kN = get_SCBquant(betaVec[b], NVec[n], muvec)
+  if(betaVec[b] < 1){
+    kN     = get_SCBquant(betaVec[b], NVec[n], muvec)
+    mu1est = "thickening"
+  }else if(b != bN){
+    kN     = log(NVec[n]) / betaVec[b]
+    mu1est = "thickening"
+  }else{
+    kN = 1
+    mu1est = NULL
+  }
 
   # Generate method list
   method = method_gen(name, SCoPEStype, mu1est, NVec[n], kN )
@@ -72,11 +73,16 @@ for(n in 1:lN){for(b in 1:bN){
   coverage[b,n]               = test$coverage
   trueDetect_scopes[b, n, ]   = test$NDetect[1, ]
   falseDetect_scopes[b, n, ]  = test$NDetect[2, ]
-  trueDetect_sidak[b, n, ]    = test$NDetect_sidak[1, ]
-  falseDetect_sidak[b, n, ]   = test$NDetect_sidak[2, ]
-  trueDetect_holm[b, n, ]     = test$NDetect_holm[1, ]
-  falseDetect_holm[b, n, ]    = test$NDetect_holm[2, ]
+
+  if(SCoPEStype == "classical"){
+    trueDetect_sidak[b, n, ]  = test$NDetect_sidak[1, ]
+    falseDetect_sidak[b, n, ] = test$NDetect_sidak[2, ]
+    trueDetect_holm[b, n, ]   = test$NDetect_holm[1, ]
+    falseDetect_holm[b, n, ]  = test$NDetect_holm[2, ]
+    trueDetect_BH[b, n, ]     = test$NDetect_BH[1, ]
+    falseDetect_BH[b, n, ]    = test$NDetect_BH[2, ]
   }
+}
 }
 
 rm(test, n, b, lN, bN, get_SCBquant, generate_muvec, generateData, kN)
